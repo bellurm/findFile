a60b0a5f5b53e805b2d730f89ac982ef3210de03
Cyber Worm <72599968+bellurm@users.noreply.github.com>
2023-05-10 08:40:03 -0400

    Update findFile.py

--- findFile.py
+++ findFile.py
@@ -1,10 +1,13 @@
-# For the Windows users.
 # This program will find your looking for file in the you entered path.
 # This program explained in my website: 'https://blog-cyberworm.com/blog/os-py'.
 import os
 
-for dirpath, dirNames, fileNames in os.walk("C:/"): #You can change the target path like 'D:/'.
+path = input("Where are you looking for the file? [eg: C:/, D:/, /root, /kali etc.] > ")
+get_file = input("What is the file name? > ")
+for dirpath, dirNames, fileNames in os.walk(path):
     for item in fileNames:
-        if item == "CHANGE ME - File Name":
-            print(dirpath)
+        if item == get_file:
+            print(f"[FOUND] '{get_file}' is here: {dirpath}")
             exit()
+            
+print(f"[NOT FOUND] '{get_file}' is not found. You can look for it in another path.")
